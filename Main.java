51d79fea72f2b767e04bbda5dca668396bdd56ac
Nathan <72050032+nATHANnSil@users.noreply.github.com>
2022-09-26 12:05:59 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyIO.setCharset("UTF-8");
+		
+		Jogo game = new Jogo();
+		
+		game.ler();
+		
+		String leitura;
+		
+		leitura = MyIO.readString();
+
+
+	}
+
+}
